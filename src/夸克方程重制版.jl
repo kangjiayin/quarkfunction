881d1b7eb5426d130c10d11243d6ec1fa47f0e1e
kangjiayin <891124092@qq.com>
2021-08-21 02:17:33 +0800

    "稳定版1"

--- src/夸克方程重制版.jl
+++ src/夸克方程重制版.jl
@@ -2,6 +2,8 @@
 #2021.08.10
 include("/Users/kjy/Desktop/program/julia/module/GaussQuad/src/gaussQuad.jl")
 include("/Users/kjy/Desktop/program/julia/module/GaussQuad/src/gaussmesh.jl")
+using JLD2
+using Plots
 ###参数列表
 const mt=0.5;
 const τ=ℯ^2-1;
@@ -13,14 +15,14 @@ const rm=12/(33 - 2*Nf);
 const m = 0.003;
 
 step = 5;
-intstep = 2^7;
+intstep = 2^9;
 cutup = 10^4+0.;
 cutdown = 10^(-4);
 
 # 分配需要变量的内存
 z1 = 1.::Float64;
 z2 = 1.::Float64;
-z4 = 1.::Float64;
+z4 = 1.5::Float64;
 A=Array{Float64}(undef,intstep,1)
 B=Array{Float64}(undef,intstep,1)
 # 点与权重
@@ -46,8 +48,8 @@ Threads.@threads for i= 1:intstep+1
         end
         q2=k[j]
         kdotq(z)=sqrt(k2*q2)*z
-        IntA[i,j]=gausschebyshevint64(z->D(k2+q2-2kdotq(z))*(3*k2*kdotq(z)+3*q2*kdotq(z)-2*k2*q2-4*(kdotq(z))^2)/k2/(k2+q2-2kdotq(z)))
-        IntB[i,j]=gausschebyshevint64(z->D(k2+q2-sqrt(k2*q2)z))
+        IntA[i,j]=(4/3)*gausschebyshevint64(z->D(k2+q2-2*kdotq(z))*(3*k2*kdotq(z)+3*q2*kdotq(z)-2*k2*q2-4*(kdotq(z))^2)/k2/(k2+q2-2*kdotq(z)))
+        IntB[i,j]=(4/3)*gausschebyshevint64(z->D(k2+q2-2*kdotq(z)))
     end
 end
 #########角度积分完结#################### 
@@ -96,27 +98,27 @@ st=0
 z2old=0.
 z4old=0.
 
-k19sumA, k19sumB=renormalpoint()
-# while maximum(Δ)>0.0000000001 && abs(1-z2old/z2)>10^-5 && abs(1-z4old/z4)>10^-5
-#     global A, B, Δ, st, z2, z4, z2old, z4old
-#     st+=1
-#     k19sumA, k19sumB=renormalpoint()
-#     z2old=z2
-#     z4old=z4
-#     z2=(-1+sqrt(1+4*k19sumA))/(2*k19sumA)
-#     z4=(m-z2^2*k19sumB)/m
-#     FA=[FAf(i) for i=1:(intstep)]
-#     FB=[FBf(i) for i=1:(intstep)]
-#     jacobiAA=[jacobifAA(i,j) for i=1:(intstep),j=1:(intstep)]
-#     jacobiAB=[jacobifAB(i,j) for i=1:(intstep),j=1:(intstep)]
-#     jacobiBA=[jacobifBA(i,j) for i=1:(intstep),j=1:(intstep)]
-#     jacobiBB=[jacobifBB(i,j) for i=1:(intstep),j=1:(intstep)]
-#     jacobi=[jacobiAA jacobiAB; jacobiBA jacobiBB]
-#     Δ=jacobi\[FA; FB]
-#     A-=Δ[1:(intstep)]
-#     B-=Δ[(intstep+1):(2intstep)]
-# end
+# k19sumA, k19sumB=renormalpoint()
+while maximum(Δ)>10^-5 && abs(1-z2old/z2)>10^-5 && abs(1-z4old/z4)>10^-5
+    global A, B, Δ, st, z2, z4, z2old, z4old
+    st+=1
+    k19sumA, k19sumB=renormalpoint()
+    z2old=z2
+    z4old=z4
+    z2=(-1+sqrt(1+4*k19sumA))/(2*k19sumA)
+    z4=(m-z2^2*k19sumB)/m
+    FA=[FAf(i) for i=1:(intstep)]
+    FB=[FBf(i) for i=1:(intstep)]
+    jacobiAA=[jacobifAA(i,j) for i=1:(intstep),j=1:(intstep)]
+    jacobiAB=[jacobifAB(i,j) for i=1:(intstep),j=1:(intstep)]
+    jacobiBA=[jacobifBA(i,j) for i=1:(intstep),j=1:(intstep)]
+    jacobiBB=[jacobifBB(i,j) for i=1:(intstep),j=1:(intstep)]
+    jacobi=[jacobiAA jacobiAB; jacobiBA jacobiBB]
+    Δ=jacobi\[FA; FB]
+    A-=Δ[1:(intstep)]
+    B-=Δ[(intstep+1):(2intstep)]
+end
 
-
+# jldsave("/Users/kjy/Desktop/program/julia/Gamma5/data/ABk128.jld2";A, B, k)
 #plot(k,A,scale=:log10,title="A")
-#plot(w,B,scale=:log10,title="B")
+#plot(k,B,scale=:log10,title="B")
